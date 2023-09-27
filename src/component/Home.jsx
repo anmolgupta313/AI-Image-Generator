8e2f627d254ee41ccd9b36ee8c7cf6a48821542c
Anmol Gupta <anmol.gupta2180@gmail.com>
2023-09-27 16:07:55 -0400

    I have found a bug while setting a image url after calling an api

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -3,76 +3,52 @@ import "../component/Home.css";
 import { React, useEffect, useState } from "react";
 import logo from "./../logo.svg";
 
-
 export default function Home() {
   const [imageUrl, setImageurl] = useState("");
   const [inputValue, setInputValue] = useState("");
-const[deafault,setDefault]= useState("false")
+
   function onChange(e) {
     setInputValue(e.target.value);
   }
 
+  // useEffect(() => {
+  //   // setImageurl("/")
+  //   // console.log(imageUrl,"url")
+  // }, [imageUrl]);
+
   async function fetchData() {
-    // try {
-    //   const fetchImageData = await fetch(
-    //     "https://open-ai21.p.rapidapi.com/texttoimage2",
-    //     {
-    //       method: "POST",
-    //       headers: {
-    //         "content-type": "application/x-www-form-urlencoded",
-    //         "X-RapidAPI-Key":
-    //           "b9076a1794msh0313b8f4ae9404fp1b23a5jsnfb26084b05be",
-    //         "X-RapidAPI-Host": "open-ai21.p.rapidapi.com",
-    //       },
+    const encodedParams = new URLSearchParams();
+    encodedParams.set("text", `${inputValue}`);
 
-    //       body: JSON.stringify({
-    //         prompt: `${inputValue}`,
-    //         n: 1,
-    //         size: "512*512",
-    //       }),
-    //     }
-    //   );
+    const url = "https://open-ai21.p.rapidapi.com/texttoimage2";
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        "X-RapidAPI-Key": "b9076a1794msh0313b8f4ae9404fp1b23a5jsnfb26084b05be",
+        "X-RapidAPI-Host": "open-ai21.p.rapidapi.com",
+      },
+      body: encodedParams,
+    };
 
-    //   const response = await fetchImageData.json();
-
-    //   console.log(response);
-    // } catch (err) {
-    //   return err;
-    // }
-
-    // const fetch = require('node-fetch');
-
-const encodedParams = new URLSearchParams();
-encodedParams.set('text', `${inputValue}`);
-
-const url = 'https://open-ai21.p.rapidapi.com/texttoimage2';
-const options = {
-  method: 'POST',
-  headers: {
-    'content-type': 'application/x-www-form-urlencoded',
-    'X-RapidAPI-Key': 'b9076a1794msh0313b8f4ae9404fp1b23a5jsnfb26084b05be',
-    'X-RapidAPI-Host': 'open-ai21.p.rapidapi.com'
-  },
-  body: encodedParams
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-	setImageurl(result.url)
-console.log(imageUrl,"url")
-    // setDefault(true)
-} catch (error) {
-	console.error(error);
-}
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      // console.log(result, "result");
+      setImageurl(result.url);
+ 
+    } catch (error) {
+      console.error(error);
+    }
   }
+  console.log(imageUrl, "url");
   return (
     <div className="home-main-div">
       <div className="main-heading">
         <h1>AI Image Generator</h1>
       </div>
       <div className="img-div">
-        <img src={logo} alt="" />
+       {imageUrl?( <img src={imageUrl}  />) :null}
       </div>
       <div className="input-div">
         <input
